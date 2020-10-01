8d1b45f996c0147b596b48c3adf8d8d3f58663bc
nntucao <ngtu.caong@gmail.com>
2020-10-02 00:06:18 +0200

    pulled data from api

--- src/App.js
+++ src/App.js
@@ -1,24 +1,88 @@
-import React from 'react';
-import logo from './logo.svg';
+import { FormControl, Select, MenuItem, Card, CardContent } from '@material-ui/core';
+import React, { useState, useEffect } from 'react';
+import InfoBox from './InfoBox'; 
+import Map from './Map'; 
 import './App.css';
 
 function App() {
+  const [countries, setCountries] = useState([]); 
+  const [country, setCountry] = useState('worldwide');
+  const [countryInfo, setCountryInfo] = useState({});
+
+  useEffect(() => {
+    fetch("https://disease.sh/v3/covid-19/all")
+    .then(response => response.json())
+    .then(data => {
+      setCountryInfo(data)
+    });
+  }, []);
+
+  useEffect(() => {
+    // async -> send a request, wait for it
+    const getCountriesData = async () => {
+      await fetch ("https://disease.sh/v3/covid-19/countries")
+      .then((response) => response.json())
+      .then((data) => {
+        const countries = data.map((country) => (
+          {
+            name: country.country, 
+            value: country.countryInfo.iso2,
+          }
+        )); 
+        setCountries(countries);
+      })
+    }; 
+    getCountriesData();
+  }, []);
+
+  const onCountryChange = async (event) => {
+    const countryCode = event.target.value; 
+    const url = countryCode === 'worldwide' ? "https://disease.sh/v3/covid-19/all" : `https://disease.sh/v3/covid-19/countries/${countryCode}`;
+
+    await fetch (url)
+      .then((response) => response.json())
+      .then((data) => {
+        setCountry(countryCode);
+        setCountryInfo(data); 
+      });
+  };
+
+  console.log("COUNTRY INFO >>>", countryInfo);
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+
+      <div className="app__left">
+          <div className="app__header">
+          <h1>COVID-19 TRACKER</h1>
+            <FormControl className="app__dropdown">
+              <Select variant="outlined" onChange={onCountryChange} value={country}>
+                <MenuItem value="worldwide">Worldwide</MenuItem>
+                  {
+                    countries.map(country => (
+                      <MenuItem value={country.value}>{country.name}</MenuItem>
+                    ))
+                  }
+              </Select>
+            </FormControl>
+          </div>
+          <div className="app__stats">
+            <InfoBox title="Coronavirus Cases" cases={countryInfo.todayCases} total={countryInfo.cases} />
+            <InfoBox title="Recovered" cases={countryInfo.todayRecovered} total={countryInfo.recovered} />
+            <InfoBox title="Deaths" cases={countryInfo.todayDeaths} total={countryInfo.deaths} />
+          </div>
+          <Map />
+        </div>
+
+        <Card className="app__right">
+          <CardContent>
+            <h3>Cases by Country</h3>
+
+            <h3>Worldwide new cases</h3>
+
+          </CardContent>
+        </Card>
+
     </div>
   );
 }
---
+++ src/Map.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Map() {
+    return (
+        <div className="map">
+            Im a map
+        </div>
+    )
+}
+
+export default Map
